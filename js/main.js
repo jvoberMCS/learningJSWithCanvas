2aa93787136e2cf1a96be119d0aa1b2dc070865f
jvoberMCS <114746137+jvoberMCS@users.noreply.github.com>
2026-03-04 09:56:35 -0500

    Rollback

--- js/main.js
+++
@@ -1,6 +0,0 @@
-import { initCanvas } from './canvas.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-    const canvas = document.getElementById('myCanvas');
-    initCanvas(canvas);
-});
